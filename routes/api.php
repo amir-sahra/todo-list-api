0c157042b5328a94cb333d94bf64188079f990af
amir-sahra <sahranavard.amir@outlook.com>
2024-10-18 14:27:03 +0330

    feat(response): custom response

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+    return Response::api('hi');
+});
+    //->middleware('auth:sanctum');
